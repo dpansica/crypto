32cde7747e738f8069a336238157a16d132cb139
Diego Pansica <dpansica@gmail.com>
2020-09-08 13:53:44 +0200

    command args

--- src/main/java/it/solutionsexmachina/crypto/CipherTool.java
+++ src/main/java/it/solutionsexmachina/crypto/CipherTool.java
@@ -2,6 +2,7 @@ package it.solutionsexmachina.crypto;
 
 import it.solutionsexmachina.crypto.definition.GenericCryptoModule;
 import it.solutionsexmachina.crypto.implementation.PGPCryptoModule;
+import org.apache.commons.cli.*;
 
 import java.io.File;
 import java.util.Scanner;
@@ -11,34 +12,81 @@ public class CipherTool {
 
     public static void main(String[] args) {
 
-        String channelId = UUID.randomUUID().toString();
+        Options options = new Options();
 
-        System.out.println("Channel ID: "+channelId);
+        Option channelOption = new Option("c", "channelOption", true, "input fileOption path");
+        channelOption.setRequired(false);
+        options.addOption(channelOption);
 
-        GenericCryptoModule cryptoModule = new PGPCryptoModule(channelId, "password");
+        Option passwordOption = new Option("p", "passwordOption", true, "output fileOption");
+        passwordOption.setRequired(false);
+        options.addOption(passwordOption);
 
-        GenericCryptoModule cryptoModuleToShare = new PGPCryptoModule(channelId);
+        Option textOption = new Option("t", "textOption", true, "textOption to encryptOption/decryptOption");
+        textOption.setRequired(false);
+        options.addOption(textOption);
 
-        Scanner scanner = new Scanner(System.in);
-        System.out.println("\nEnter content to encipher: ");
-        String content = scanner.nextLine();
+        Option fileOption = new Option("f", "fileOption", true, "fileOption to encryptOption/decryptOption");
+        fileOption.setRequired(false);
+        options.addOption(fileOption);
 
-        String encipher = cryptoModuleToShare.encipher(content);
+        Option encryptOption = new Option("e", "encryptOption", false, "encryptOption command");
+        encryptOption.setRequired(false);
+        options.addOption(encryptOption);
 
-        System.out.println("Encrypted with shared key: ");
-        System.out.println(encipher);
+        Option decryptOption = new Option("d", "decryptOption", false, "decryptOption command");
+        decryptOption.setRequired(false);
+        options.addOption(decryptOption);
 
-        System.out.println("Decrypted with secret key: ");
-        System.out.println(cryptoModule.decipher(encipher));
+        CommandLineParser parser = new DefaultParser();
+        HelpFormatter formatter = new HelpFormatter();
+        CommandLine cmd = null;
 
-        System.out.println("\nEnter file to encipher: ");
-        String filename = scanner.nextLine();
+        try {
+            cmd = parser.parse(options, args);
+        } catch (ParseException e) {
+            System.out.println(e.getMessage());
+            formatter.printHelp("utility-name", options);
 
-        System.out.println("Encrypted with shared key: ");
-        System.out.println(cryptoModuleToShare.encipher(new File(filename)));
+            System.exit(1);
+        }
 
-        System.out.println("Decrypted with secret key: ");
-        System.out.println(cryptoModule.decipher(new File(filename+".enc")));
+        String channel = cmd.getOptionValue("channelOption");
+        String password = cmd.getOptionValue("passwordOption");
+        String text = cmd.getOptionValue("textOption");
+        String filename = cmd.getOptionValue("fileOption");
+        boolean encrypt = cmd.hasOption("encryptOption");
+        boolean decrypt = cmd.hasOption("decryptOption");
+
+        GenericCryptoModule cryptoModule = null;
+
+        if (channel==null){
+            channel = UUID.randomUUID().toString();
+            password = UUID.randomUUID().toString();
+            new PGPCryptoModule(channel, password);
+            System.out.println("Channel  "+channel+" generated with password: "+password);
+        }
+
+        if (encrypt){
+            cryptoModule = new PGPCryptoModule(channel);
+
+            if (text!=null){
+                System.out.println(cryptoModule.encipher(text));
+            }
+            if (filename!=null){
+                System.out.println(cryptoModule.encipher(new File(filename)));
+            }
+        }
+        if (decrypt && password!=null){
+            cryptoModule = new PGPCryptoModule(channel, password);
+
+            if (text!=null){
+                System.out.println(cryptoModule.decipher(text));
+            }
+            if (filename!=null){
+                System.out.println(cryptoModule.decipher(new File(filename)));
+            }
+        }
     }
 
 }
